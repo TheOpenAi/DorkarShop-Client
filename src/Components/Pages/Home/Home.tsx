2d6f82241a3f3b090dd2f03729d41649905cc722
AnGon777 <ikhtiaj.arif@gmail.com>
2022-12-24 15:46:11 +0600

    review service & featured sections created

--- src/Components/Pages/Home/Home.tsx
+++ src/Components/Pages/Home/Home.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import FeaturedProducts from './FeaturedProducts/FeaturedProducts';
+import Review from './Reviews/Review';
+import Services from './Services/Services';
+
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             
-            {/* Feautere  Products  */}
+         <FeaturedProducts/>
             {/* categoried */}
-            {/* services  */}
+            <Services/>
+            <Review/>
             {/*  Branches*/}
             {/*  Testimonials */}
             {/*  Team member */}
