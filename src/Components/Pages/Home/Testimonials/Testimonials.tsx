7f8e8f3c94320c15844c31bbf884657f5b568bb4
radiation22 <radiationcorporation2@gmail.com>
2022-12-25 20:15:49 +0600

    testimonial add

--- src/Components/Pages/Home/Testimonials/Testimonials.tsx
+++ src/Components/Pages/Home/Testimonials/Testimonials.tsx
@@ -5,12 +5,15 @@ import "./testemonial.css"
 import "swiper/css";
 
 import "swiper/css/pagination";
-
+// import Testimonials from './Testimonials';
 // import required modules
 import { Pagination } from "swiper";
+
 const Testimonials = () => {
     return (
         <div>
+            <h2 className='text-4xl text-center font-bold'>Testimonials</h2>
+            <h3 className='text-2xl text-center'>What Client Say </h3>
              <>
       <Swiper
         slidesPerView={3}
@@ -21,9 +24,49 @@ const Testimonials = () => {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>
+        <div className="card  bg- shadow-xl">
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions">
+    <div className="avatar">
+  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+    <img src="https://placeimg.com/192/192/people" />
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="card  bg- shadow-xl">
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions">
+    <div className="avatar">
+  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+    <img src="https://placeimg.com/192/192/people" />
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+        </SwiperSlide>
+        <SwiperSlide>   <div className="card  bg- shadow-xl">
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions">
+    <div className="avatar">
+  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+    <img src="https://placeimg.com/192/192/people" />
+  </div>
+</div>
+    </div>
+  </div>
+</div></SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
         <SwiperSlide>Slide 6</SwiperSlide>
