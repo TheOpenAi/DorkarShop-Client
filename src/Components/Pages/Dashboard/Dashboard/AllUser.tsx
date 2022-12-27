20f7ad1c7f607734ecac892e6c22ab2e3e7beddd
unknown <anamika362890@gmail.com>
2022-12-27 16:50:21 +0600

    allUser

--- src/Components/Pages/Dashboard/Dashboard/AllUser.tsx
+++ src/Components/Pages/Dashboard/Dashboard/AllUser.tsx
@@ -4,7 +4,7 @@ const AllUser = () => {
     return (
         <div>
             <div>
-                <h1 className='text-4xl text-blue-900 font-bold text-center my-5'>All Users</h1>
+                <h1 className='text-4xl text-blue-900 font-bold text-center my-5'>All Sellers</h1>
 
                 <div className="overflow-x-auto">
                     <table className="table w-full">
@@ -16,70 +16,43 @@ const AllUser = () => {
 
                                 <th>Name</th>
                                 <th>Email</th>
-                                <th>Verification</th>
-                                <th>Delete</th>
+                                <th>Role</th>
+                                <th>Make Admin</th>
+
                             </tr>
                         </thead>
                         <tbody>
 
-                            <tr  >
-                                <th>1</th>
+                            <tr >
+                                <th> 1</th>
                                 <td><div className="mask mask-squircle w-12 h-12">
-                                    <img src="https://static.vecteezy.com/system/resources/previews/004/773/704/original/a-girl-s-face-with-a-beautiful-smile-a-female-avatar-for-a-website-and-social-network-vector.jpg" alt='' />
+                                    <img src="" alt="Avatar Tailwind CSS Component" />
                                 </div></td>
 
-                                <td>Hart Hagerty</td>
-                                <td>hart@gmail.com</td>
-
-                                <td>
-
+                                <td>A</td>
+                                <td>b</td>
+                                <td>c</td>
 
 
 
-                                    <label htmlFor="Confirmation-modal" className=" btn bg-gradient-to-r from-blue-800 to-blue-700 border-none">Verify</label>
+                                <td  >
 
-
-
-
-
-
-
-
-
+                                    <button className='btn  bg-gradient-to-r from-blue-800 to-blue-700 border-none'>Make Admin</button>
                                 </td>
 
-                                <td>
 
-
-
-
-                                    <label htmlFor="Confirmation-modal" className=" btn bg-gradient-to-r from-red-800 to-red-700 border-none">Delete</label>
-
-
-
-
-
-
-
-
-
-                                </td>
 
 
                             </tr>
-
 
 
                         </tbody>
                     </table>
                 </div>
 
-
-
-
-
             </div>
-        </div>
+
+        </div >
     );
 };
 
