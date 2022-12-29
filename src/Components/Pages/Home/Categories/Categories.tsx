2827ee6eb690fcf39c394ae5d9c19b84a74401da
AnGon777 <ikhtiaj.arif@gmail.com>
2022-12-29 17:04:24 +0600

    category styled

--- src/Components/Pages/Home/Categories/Categories.tsx
+++ src/Components/Pages/Home/Categories/Categories.tsx
@@ -1,6 +1,12 @@
 import Aos from 'aos';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 // import Category from './Category';
 
 const Categories = () => {
@@ -16,34 +22,52 @@ const Categories = () => {
     }, [])
     console.log(categories);
     return (
+
+
+
+
+
+
+
+
+
+
         <div>
             <div className="flex items-center justify-center h-auto bg-white pt-24">
                 <div className="flex flex-col">
 
-                    <div className="flex flex-wrap justify-center text-center mb-24" data-aos='fade-right'>
-                        <div className="w-full lg:w-6/12 px-4">
+                    <div className="flex flex-wrap justify-center text-center mb-10" data-aos='fade-right'>
+                        <div className="w-full  lg:w-6/12 px-10">
                             <h1 className=" text-4xl font-bold mb-8">
-                                All Categories
+                                Why buy from us?
                             </h1>
+                            <p className='text-gray-500 font-bold'>DorkarShop has been the best selling and most loved Open cart theme since first launch in 2013. Tried and tested by over 20K people, DorkarShop is the bes on the market today</p>
                         </div>
                     </div>
 
 
+
                     <div className="">
-                        <div className=" grid md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-10  px-4">
+                        <div className=" grid  md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-10 m-10 px-4">
                             {
                                 categories.map((category, idx) => (
                                     <Link key={category._id} to={`/sinlgecategories/${category?.title}`}>
-                                        <div className="card  w-52 bg-base-100 shadow-lg shadow-primary" data-aos='fade-up'>
-                                            <div className="card-body">
-                                                <h2 className="card-title capitalize">{category?.title}</h2>
+
+                                        <div className="card w-52 lg:w-64 bg-base-100 shadow-lg shadow-primary" data-aos='fade-up'>
+                                            <div className="relative text-center">
+                                                <img src={category?.imgUrl} alt="" className='h-full rounded-t-xl' />
+                                                <h2 className="capitalize font-semibold text-lg text-accent py-1">{category?.title}</h2>
                                             </div>
                                         </div>
+
                                     </Link>
                                 ))
                             }
                         </div>
                     </div>
+
+
+
                 </div>
 
             </div>
