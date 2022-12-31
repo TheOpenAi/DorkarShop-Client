c5ac3912e03fa22960c0d3137e4c9db276da1881
unknown <anamika362890@gmail.com>
2022-12-31 18:16:14 +0600

    pay3

--- src/Components/Payment/Payment.tsx
+++ src/Components/Payment/Payment.tsx
@@ -31,7 +31,7 @@ const Payment = () => {
 
 
             <div className=" ">
-                <h1>{order.price}</h1>
+
                 <div className="bg-white p-6  md:mx-auto">
                     <svg viewBox="0 0 24 24" className="text-green-600 w-16 h-16 mx-auto my-6">
                         <path fill="currentColor"
@@ -40,62 +40,63 @@ const Payment = () => {
                     </svg>
                     <div className="text-center">
 
-                        <h1>{order.price}</h1>
+
                         <h3 className="md:text-2xl text-base text-gray-900 font-semibold text-center">Payment Done!</h3>
                         <p className="text-gray-600 my-2">Thank you for completing your secure online payment.</p>
                         <p> Have a great day!  </p>
-                        <div className="py-10 text-center">
-                            <a href="/" className=" text-white font-semibold py-3 px-6 text-center rounded-xl transition bg-gradient-to-r from-primary to-secondary  focus:secondary">
-                                GO BACK TO HOME
-                            </a>
+                        <div className="py-11 text-center">
 
 
+
+
+                            <div>
+                                <h1 className='text-4xl text-blue-900 font-bold text-center my-5'>Order Summery</h1>
+                                <div className="overflow-x-auto">
+                                    <table className="table w-full">
+                                        <thead>
+                                            <tr>
+
+
+                                                <th>Invoice ID</th>
+                                                <th>Product Name</th>
+                                                <th>Category</th>
+                                                <th>Price</th>
+                                                <th>Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            <tr  >
+
+
+                                                <td>{order._id}</td>
+                                                <td>{order.model}</td>
+                                                <td>{order.category}</td>
+                                                <td>{order.price}</td>
+                                                <td>
+                                                    Paid
+                                                </td>
+
+                                            </tr>
+
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
 
                         </div>
                     </div>
                 </div>
             </div>
+
             <div>
 
 
-                <div>
 
-                    <div>
-                        <h1 className='text-4xl text-blue-900 font-bold text-center my-5'>Order Summery</h1>
-                        <div className="overflow-x-auto">
-                            <table className="table w-full">
-                                <thead>
-                                    <tr>
-
-
-                                        <th>Invoice ID</th>
-                                        <th>Product Name</th>
-                                        <th>Category</th>
-                                        <th>Price</th>
-                                        <th>Status</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-
-                                    <tr  >
-
-
-                                        <td>{order._id}</td>
-                                        <td>{order.model}</td>
-                                        <td>{order.category}</td>
-                                        <td>{order.price}</td>
-                                        <td>
-                                            Paid
-                                        </td>
-
-                                    </tr>
-
-
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
 
 
             </div>
