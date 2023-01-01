a03c83920dc9eed72103a12e56466d75fbf3e0e8
sojib076 <sojibds332@gmail.com>
2023-01-01 22:12:32 +0600

    breadcmp

--- src/Components/Pages/Dashboard/Dashboard/MyProduct.tsx
+++ src/Components/Pages/Dashboard/Dashboard/MyProduct.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import React, { useContext, useEffect, useState } from 'react';
 import { toast } from 'react-hot-toast';
+import { Link } from 'react-router-dom';
 import { UserContext } from '../../../../context/UserProvider';
 
 const MyProduct = () => {
@@ -41,6 +42,26 @@ const MyProduct = () => {
 
     return (
         <div>
+                 <div className="text-sm breadcrumbs">
+                <ul>
+                    <li>
+                        <Link to={'/home'}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-2 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
+                            home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'/dashboard'}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-2 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
+                            dashboard
+                        </Link>
+                    </li>
+                    <li>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-2 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
+                        my products
+                    </li>
+                </ul>
+            </div>
             <h1 className='text-4xl text-blue-900 font-bold text-center my-5'>My Products </h1>
 
             <div className="overflow-x-auto">
