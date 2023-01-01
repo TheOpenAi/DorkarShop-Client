66d4cc2da5af5e39bc41101ecdf50484e2c385d2
Abu Siam <siamcse01@gmail.com>
2023-01-01 21:09:40 +0600

    routes added

---
+++ src/Routes/SellerRoutes.tsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router';
+import Loading from '../Components/Pages/Loading/Loading';
+import { UserContext } from '../context/UserProvider';
+
+const SellerRoutes = ({children}:any) => {
+    const userContext = useContext(UserContext)
+    const location = useLocation();
+
+    if (userContext?.loading) {
+        return <Loading />
+    }
+
+    if (userContext?.user?.email) {
+        return children;
+    }
+    return <Navigate to='/login' state={{ from: location }} replace></Navigate>
+};
+
+export default SellerRoutes;
